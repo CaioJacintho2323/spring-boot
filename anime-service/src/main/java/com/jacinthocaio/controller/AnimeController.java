a8982d8b716be5fc3fc5630ca4582cad080cb69a
CaioJacintho2323 <jacinthocaio@gmail.com>
2025-01-16 12:19:56 -0300

    feat: getAnimes

--- anime-service/src/main/java/com/jacinthocaio/controller/AnimeController.java
+++ anime-service/src/main/java/com/jacinthocaio/controller/AnimeController.java
@@ -1,22 +1,19 @@
 package com.jacinthocaio.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
-public class HelloController {
+@RequestMapping(value = {"v1/animes","v1/animes/"})
+public class AnimeController {
 
-
-    @GetMapping("hi")
-    public String hi(){
-        return "OMAE WA MOU SHINDE IRU";
+    @GetMapping()
+    public List<String> returnAnimes(){
+        return List.of("Anime 1", "Anime 2", "Anime 3", "Anime 4", "Anime 5");
     }
-
-
-
 }
